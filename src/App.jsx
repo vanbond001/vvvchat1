7e0f134f8b4528d3978cea55ae2c941802e31bd8
vinrai007 <129852101+vinrai007@users.noreply.github.com>
2024-01-24 17:24:20 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import axios from "axios";
+import { UserContextProvider } from "./UserContext";
+import { MessagesProvider } from './MessageContext';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ContactPage from "./ContactPage";
+import MessagePage from "./MessagePage";
+import Rotes from "./Routes";  // Assuming this is the correct import
+import './App.css';
+
+
+function App() {
+  axios.defaults.baseURL = 'http://localhost:4040';
+  axios.defaults.withCredentials = true;
+
+  return (
+    <UserContextProvider>
+      <MessagesProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Rotes />} />
+          <Route path="/contacts" element={<ContactPage />} />
+          <Route path="/messages/:selectedUserId" element={<MessagePage />} />
+        </Routes>
+        </Router>
+      </MessagesProvider>
+    </UserContextProvider>
+  );
+}
+
+export default App;
---
+++ src/MessagePage2.jsx
@@ -0,0 +1,259 @@
+// MessagePage.js
+import React, { useContext, useEffect, useRef, useState } from 'react';
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
+import { UserContext } from './UserContext';
+import { MessagesContext } from './MessageContext';
+
+import {uniqBy} from "lodash";
+import Logo from "./Logo";
+import Contact from "./Contact";
+
+
+const MessagePage = () => {
+  // const { setMessages, messages } = useContext(UserContext);
+   const { setMessages, messages } = useContext(MessagesContext);
+  const divUnderMessages = useRef();
+  const { selectedUserId } = useParams();
+
+    const [onlinePeople,setOnlinePeople] = useState({});
+  const {username,id,setId,setUsername} = useContext(UserContext);
+  const [ws,setWs] = useState(null);
+  const [offlinePeople, setOfflinePeople] = useState({});
+  const [newMessageText, setNewMessageText] = useState('');
+
+  // useEffect(() => {
+  //   if (selectedUserId) {
+  //     axios.get(`/messages/${selectedUserId}`).then((res) => {
+  //       setMessages(res.data);
+  //     });
+  //   }
+  // }, [selectedUserId, setMessages]);
+
+  // useEffect(() => {
+  //   const div = divUnderMessages.current;
+  //   if (div) {
+  //     div.scrollIntoView({ behavior: 'smooth', block: 'end' });
+  //   }
+  // }, [messages]);
+ 
+
+  useEffect(() => {
+    connectToWs();
+  }, [selectedUserId]);
+  function connectToWs() {
+    const ws = new WebSocket('ws://localhost:4040');
+    setWs(ws);
+    ws.addEventListener('message', handleMessage);
+    ws.addEventListener('close', () => {
+      setTimeout(() => {
+        console.log('Disconnected. Trying to reconnect.');
+        connectToWs();
+      }, 1000);
+    });
+  }
+  function showOnlinePeople(peopleArray) {
+    const people = {};
+    peopleArray.forEach(({userId,username}) => {
+      people[userId] = username;
+    });
+    setOnlinePeople(people);
+  }
+  function handleMessage(ev) {
+    const messageData = JSON.parse(ev.data);
+    console.log({ev,messageData});
+    if ('online' in messageData) {
+      showOnlinePeople(messageData.online);
+    } else if ('text' in messageData) {
+      if (messageData.sender === selectedUserId) {
+        setMessages(prev => ([...prev, {...messageData}]));
+      }
+    }
+  }
+  function logout() {
+    axios.post('/logout').then(() => {
+      setWs(null);
+      setId(null);
+      setUsername(null);
+    });
+  }
+  function sendMessage(ev, file = null) {
+    if (ev) ev.preventDefault();
+    ws.send(JSON.stringify({
+      recipient: selectedUserId,
+      text: newMessageText,
+      file,
+    }));
+    if (file) {
+      axios.get('/messages/'+selectedUserId).then(res => {
+        setMessages(res.data);
+      });
+    } else {
+      setNewMessageText('');
+      setMessages(prev => ([...prev,{
+        text: newMessageText,
+        sender: id,
+        recipient: selectedUserId,
+        _id: Date.now(),
+      }]));
+    }
+  }
+  function sendFile(ev) {
+    const reader = new FileReader();
+    reader.readAsDataURL(ev.target.files[0]);
+    reader.onload = () => {
+      sendMessage(null, {
+        name: ev.target.files[0].name,
+        data: reader.result,
+      });
+    };
+  }
+
+  useEffect(() => {
+    const div = divUnderMessages.current;
+    if (div) {
+      div.scrollIntoView({behavior:'smooth', block:'end'});
+    }
+  }, [messages]);
+
+  useEffect(() => {
+    axios.get('/people').then(res => {
+      const offlinePeopleArr = res.data
+        .filter(p => p._id !== id)
+        .filter(p => !Object.keys(onlinePeople).includes(p._id));
+      const offlinePeople = {};
+      offlinePeopleArr.forEach(p => {
+        offlinePeople[p._id] = p;
+      });
+      setOfflinePeople(offlinePeople);
+    });
+  }, [onlinePeople]);
+
+  useEffect(() => {
+    if (selectedUserId) {
+      axios.get('/messages/'+selectedUserId).then(res => {
+        setMessages(res.data);
+      });
+    }
+  }, [selectedUserId]);
+
+  const onlinePeopleExclOurUser = {...onlinePeople};
+  delete onlinePeopleExclOurUser[id];
+
+  const messagesWithoutDupes = uniqBy(messages, '_id');  
+
+  return (
+    <div className='relative h-screen p-2 bg-gray-200'>
+      <div className='flex flex-col'>
+      {/* <h1>Message Page for User {selectedUserId}</h1> */}
+        <div className="flex-grow">
+{Object.keys(onlinePeopleExclOurUser)
+  .filter(userId => userId === selectedUserId)
+  .map(userId => (
+    <Contact
+      key={userId}
+      id={userId}
+      online={true}
+      username={onlinePeopleExclOurUser[userId]}
+            selected={userId === selectedUserId}
+      onClick={() => {setSelectedUserId(userId); console.log({userId})}}
+    />
+))}
+{Object.keys(offlinePeople)
+  .filter(userId => userId === selectedUserId)
+  .map(userId => (
+    <Contact
+      key={userId}
+      id={userId}
+      online={false}
+      username={offlinePeople[userId].username}
+      onClick={() => setSelectedUserId(userId)}
+      selected={userId === selectedUserId}
+    />
+))}
+        </div>
+        <div>
+      <div  className='relative h-full'>
+      <div className="overflow-y-scroll absolute top-0 left-0 right-0 bottom-2">
+        {messages && messages.map((message) => (
+          <div key={message._id} className={message.sender === selectedUserId ? 'text-left' : 'text-right'}>
+            <div
+              className={`text-left inline-block p-2 my-2 rounded-md text-sm ${
+                message.sender === selectedUserId ? 'bg-white text-gray-500' : 'bg-blue-500 text-white'
+              }`}
+            >
+              {message.text}
+              {message.file && (
+                <div className="">
+                  <a target="_blank" className="flex items-center gap-1 border-b" href={axios.defaults.baseURL + '/uploads/' + message.file}>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-4 h-4">
+                      <path
+                        fillRule="evenodd"
+                        d="M18.97 3.659a2.25 2.25 0 00-3.182 0l-10.94 10.94a3.75 3.75 0 105.304 5.303l7.693-7.693a.75.75 0 011.06 1.06l-7.693 7.693a5.25 5.25 0 11-7.424-7.424l10.939-10.94a3.75 3.75 0 115.303 5.304L9.097 18.835l-.008.008-.007.007-.002.002-.003.002A2.25 2.25 0 015.91 15.66l7.81-7.81a.75.75 0 011.061 1.06l-7.81 7.81a.75.75 0 001.054 1.068L18.97 6.84a2.25 2.25 0 000-3.182z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                    {message.file}
+                  </a>
+                </div>
+              )}
+            </div>
+          </div>
+        ))}
+        <div ref={divUnderMessages}></div>
+          </div>
+          </div>
+      <div className='absolute bottom-0 w-full'>
+         {!!selectedUserId && (
+  <form className="flex gap-2 " onSubmit={sendMessage}>
+    <input
+      type="text"
+      value={newMessageText}
+      onChange={(ev) => setNewMessageText(ev.target.value)}
+      placeholder="Type your message here"
+      className="flex-grow border rounded-sm p-2 focus:outline-none focus:ring focus:border-blue-300"
+    />
+    <label className="bg-blue-200 p-2 text-gray-600 cursor-pointer rounded-sm border border-blue-200">
+      <input type="file" className="hidden" onChange={sendFile} />
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        fill="currentColor"
+        className="w-6 h-6"
+      >
+        <path
+          fillRule="evenodd"
+          d="M18.97 3.659a2.25 2.25 0 00-3.182 0l-10.94 10.94a3.75 3.75 0 105.304 5.303l7.693-7.693a.75.75 0 011.06 1.06l-7.693 7.693a5.25 5.25 0 11-7.424-7.424l10.939-10.94a3.75 3.75 0 115.303 5.304L9.097 18.835l-.008.008-.007.007-.002.002-.003.002A2.25 2.25 0 015.91 15.66l7.81-7.81a.75.75 0 011.061 1.06l-7.81 7.81a.75.75 0 001.054 1.068L18.97 6.84a2.25 2.25 0 000-3.182z"
+          clipRule="evenodd"
+        />
+      </svg>
+    </label>
+    <button
+      type="submit"
+      className="bg-blue-500 p-2 text-white rounded-sm hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-300"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="w-6 h-6"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5"
+        />
+      </svg>
+    </button>
+  </form>
+)}
+        </div>
+        </div>
+        </div>
+    </div>
+  );
+};
+
+export default MessagePage;
